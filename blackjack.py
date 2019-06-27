d12460d992b3c20aa75ad24fa2450d1082da16ff
Charlie Parker <censoredcharlie@gmail.com>
2019-06-27 14:13:48 +0100

    Added Dealer class

--- blackjack.py
+++ blackjack.py
@@ -2,11 +2,9 @@ from random import shuffle
 
 class Shoe:
 
-    def __init__(self, shoe_size=1, deck=["A","2","3","4","5","6","7","8","9","T","J","Q","K"]*4):
+    def __init__(self, shoe_size=1):
         self.burn = []
-        self.deck = deck
-        self.shoe = deck * shoe_size
-        self.size = len(deck)
+        self.shoe = ["A","2","3","4","5","6","7","8","9","T","J","Q","K"]*4 * shoe_size
         
     def shuffle_shoe(self):
         self.shoe = self.shoe + self.burn
@@ -29,27 +27,37 @@ class Hand:
         
     def evaluate_score(self):
         score = 0
-        soft = True
-        hard = True
-        cardDict = {"A":11,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"T":10,"J":10,"Q":10,"K":10}
-        
+        soft_aces = 0
+        self.cardDict = {"A":11,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"T":10,"J":10,"Q":10,"K":10}
         for card in self.cards:
-            score+=cardDict[card]
+            score+=self.cardDict[card]
             if(card == "A"):
-                hard = False
+                soft_aces += 1
             if(score > 21):
-                if(not hard and soft):
-                    soft = False
+                if(soft_aces > 0):
+                    soft_aces -= 1
                     score -= 10
         return score
+        
+    def is_blackjack(self):
+        return (len(self.cards)==2 and self.evaluate_score() == 21)
         
     def pretty_print(self):
         pretty_string = ""
         for card in self.cards:
             pretty_string += card + " "
-            
         pretty_string += "\tScore: " + str(self.evaluate_score())
         print pretty_string
         
-
+        
+class Dealer:
+    
+    def __init__(self, limit=17):
+        self.limit = limit
+        self.hand = Hand()
+        
+    def play_turn(self, deck):
+        if(self.limit > self.hand.evaluate_score()):
+            self.hand.draw_card(deck)
+        
     
